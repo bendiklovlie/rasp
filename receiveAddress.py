012e9bc9cca1f40d2173d2f9843f8ac3eb4b922c
Bendik <bendil@stud.ntnu.no>
2022-02-16 22:09:36 +0100

    udp receive and send ip address

---
+++ receiveAddress.py
@@ -0,0 +1,17 @@
+import socket
+
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+ip_arr = local_ip.split('.')
+ip_arr[3] = '255'
+UDP_IP = '.'.join(ip_arr)
+UDP_PORT = 9001
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
---
+++ sendAddress.py
@@ -0,0 +1,26 @@
+import socket
+from threading import local
+from time import sleep
+
+
+
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+ip_arr = local_ip.split('.')
+ip_arr[3] = '255'
+UDP_IP = '.'.join(ip_arr)
+UDP_PORT = 9001
+print(hostname)
+print(local_ip)
+MESSAGE = local_ip.encode('UTF-8')
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+while(True):
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    print("sent")
+    sleep(5)
